399c1b7f5f67ff9082176a3a2b5a46b5098a9523
gabriel.ge <geym@shicaidai.com>
2017-03-10 14:47:47 +0800

    init

---
+++ dashboard/src/main/java/com/itmad/study/cloud/dashboard/HystrixDashboardApplication.java
@@ -0,0 +1,16 @@
+package com.itmad.study.cloud.dashboard;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * Created by Gabriel.Ge on 2017-03-03.
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(HystrixDashboardApplication.class).web(true).run(args);
+    }
+}
